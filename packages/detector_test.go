a9e32ae2673d81665d47718c5704c27c5c2341b7
Mike Hoskins <mrh@devopsdreams.io>
2020-10-11 13:45:47 -0400

    mock os-release content vs which exit code

--- packages/detector_test.go
+++ packages/detector_test.go
@@ -15,101 +15,80 @@ package packages
 
 import (
 	"errors"
-	"fmt"
-	"os"
-	"os/exec"
 	"testing"
 
 	"github.com/sirupsen/logrus"
 	"github.com/sonatype-nexus-community/ahab/logger"
 )
 
-var testType string
-
-func fakeExecCommand(command string, args ...string) *exec.Cmd {
-	cs := []string{"-test.run=TestHelperProcess", "--", command}
-	cs = append(cs, args...)
-	cmd := exec.Command(os.Args[0], cs...)
-	cmd.Env = []string{"GO_WANT_HELPER_PROCESS=" + testType}
-	return cmd
-}
-
-func TestHelperProcess(t *testing.T) {
-	fmt.Println(">>> TEST PROCESS <<<<")
-	if os.Getenv("GO_WANT_HELPER_PROCESS") == "notinstalled" ||
-		os.Getenv("GO_WANT_HELPER_PROCESS") == "yuminstalled" ||
-		os.Getenv("GO_WANT_HELPER_PROCESS") == "dpkgqueryinstalled" ||
-		os.Getenv("GO_WANT_HELPER_PROCESS") == "apkinstalled" ||
-		os.Getenv("GO_WANT_HELPER_PROCESS") == "dnfinstalled" {
-		expectedProgram := os.Args[4]
-		if os.Getenv("GO_WANT_HELPER_PROCESS") == "notinstalled" {
-			fmt.Println(">>> NOT INSTALLED CODE ... ALWAYS RETURN 1 <<<<")
-			os.Exit(1)
-		} else if os.Getenv("GO_WANT_HELPER_PROCESS") == "yuminstalled" {
-			fmt.Fprintln(os.Stdout, ">>> YUM CODE <<<<")
-			if expectedProgram == "yum" {
-				fmt.Fprintln(os.Stdout, ">>> YUM CODE : ASKING FOR YUM ... ITS INSTALLED <<<<")
-				os.Exit(0)
-			} else {
-				fmt.Fprintln(os.Stdout, ">>> YUM CODE : ASKING FOR SOMETHING ELSE ... ITS NOT INSTALLED <<<<")
-				os.Exit(1)
-			}
-		} else if os.Getenv("GO_WANT_HELPER_PROCESS") == "dpkgqueryinstalled" {
-			fmt.Println(">>> DKPG-QUERY CODE <<<<")
-			if expectedProgram == "dpkg-query" {
-				fmt.Fprintln(os.Stdout, ">>> DKPG-QUERY CODE : ASKING FOR APT ... ITS INSTALLED <<<<")
-				os.Exit(0)
-			} else {
-				fmt.Fprintln(os.Stdout, ">>> DKPG-QUERY CODE : ASKING FOR SOMETHING ELSE ... ITS NOT INSTALLED <<<<")
-				os.Exit(1)
-			}
-		} else if os.Getenv("GO_WANT_HELPER_PROCESS") == "apkinstalled" {
-			fmt.Println(">>> APK CODE <<<<")
-			if expectedProgram == "apk" {
-				fmt.Fprintln(os.Stdout, ">>> APK CODE : ASKING FOR APK ... ITS INSTALLED <<<<")
-				os.Exit(0)
-			} else {
-				fmt.Fprintln(os.Stdout, ">>> APK CODE : ASKING FOR SOMETHING ELSE ... ITS NOT INSTALLED <<<<")
-				os.Exit(1)
-			}
-		} else if os.Getenv("GO_WANT_HELPER_PROCESS") == "dnfinstalled" {
-			fmt.Println(">>> DNF CODE <<<<")
-			if expectedProgram == "dnf" {
-				fmt.Fprintln(os.Stdout, ">>> DNF CODE : ASKING FOR DNF ... ITS INSTALLED <<<<")
-				os.Exit(0)
-			} else {
-				fmt.Fprintln(os.Stdout, ">>> DNF CODE : ASKING FOR SOMETHING ELSE ... ITS NOT INSTALLED <<<<")
-				os.Exit(1)
-			}
-		}
-	} else {
-		fmt.Println(">>> RETURNING <<<<")
-		return
-	}
-}
-
 func TestDetectPackageManager(t *testing.T) {
 	tests := map[string]struct {
-		expectedInstalledPackageManager string
-		expectedResult                  string
-		expectedErr                     error
+		release        Release
+		expectedResult string
+		expectedErr    error
 	}{
-		"apk":            {expectedInstalledPackageManager: "apkinstalled", expectedResult: "apk", expectedErr: nil},
-		"yum":            {expectedInstalledPackageManager: "yuminstalled", expectedResult: "yum", expectedErr: nil},
-		"dnf":            {expectedInstalledPackageManager: "dnfinstalled", expectedResult: "dnf", expectedErr: nil},
-		"dpkg-query":     {expectedInstalledPackageManager: "dpkgqueryinstalled", expectedResult: "dpkg", expectedErr: nil},
-		"none installed": {expectedInstalledPackageManager: "notinstalled", expectedResult: "", expectedErr: errors.New(SupportedPackageManagers)},
+		"alpine": {
+			release: Release{
+				ID:      []byte("alpine"),
+				Version: []byte(""),
+			},
+			expectedResult: "apk",
+			expectedErr:    nil,
+		},
+		"centos_new": {
+			release: Release{
+				[]byte("centos"),
+				[]byte("8"),
+			},
+			expectedResult: "dnf",
+			expectedErr:    nil,
+		},
+		"centos_old": {
+			release: Release{
+				[]byte("centos"),
+				[]byte("7"),
+			},
+			expectedResult: "yum",
+			expectedErr:    nil,
+		},
+		"fedora_new": {
+			release: Release{
+				[]byte("fedora"),
+				[]byte("32"),
+			},
+			expectedResult: "dnf",
+			expectedErr:    nil,
+		},
+		"fedora_old": {
+			release: Release{
+				[]byte("fedora"),
+				[]byte("22"),
+			},
+			expectedResult: "yum",
+			expectedErr:    nil,
+		},
+		"debian": {
+			release: Release{
+				[]byte("debian"),
+				[]byte(""),
+			},
+			expectedResult: "dpkg",
+			expectedErr:    nil,
+		},
+		"none installed": {
+			release: Release{
+				[]byte("unsupported"),
+				[]byte(""),
+			},
+			expectedResult: "",
+			expectedErr:    errors.New(SupportedPackageManagers),
+		},
 	}
 
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
-			testType = test.expectedInstalledPackageManager
-
-			execCommand = fakeExecCommand
-			defer func() { execCommand = exec.Command }()
-
 			log, _ := logger.GetLogger(logrus.TraceLevel)
-			actualResult, actualErr := DetectPackageManager(log)
+			actualResult, actualErr := DetectPackageManager(log, test.release)
 
 			if actualResult != test.expectedResult {
 				t.Errorf("Expected %q, got %q", test.expectedResult, actualResult)
